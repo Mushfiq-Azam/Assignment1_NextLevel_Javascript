e856eedb96ae80de01b76a9b99b139efcfb59378
Mushfiq-Azam <mushfiq.azam7860@gmail.com>
2026-09-02 21:52:06 +0600

    add getDayType function to determine day type based on input

---
+++ ques02.js
@@ -0,0 +1,20 @@
+
+function getDayType(day_name) {
+    let x = day_name.toLowerCase();
+
+    switch (x) {
+        case "friday":
+        case "saturday":
+            return "Weekend";
+
+        case "sunday":
+        case "monday":
+        case "tuesday":
+        case "wednesday":
+        case "thursday":
+            return "Working Day";
+
+        default:
+            return "Invalid Day";
+    }
+}
