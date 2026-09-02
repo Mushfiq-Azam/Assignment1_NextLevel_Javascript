53346aa97dc481d2ab2390d8ebfd5c37635afc3e
Mushfiq-Azam <mushfiq.azam7860@gmail.com>
2026-09-02 21:48:50 +0600

    refactor describeValue function and add comprehensive test cases

--- ques01.js
+++ ques01.js
@@ -7,5 +7,16 @@ function describeValue(x){
         y="falsy";
     }
     return type+"|"+y;
-    console.log(type+"|"+y);
+    
 }
+
+// Testing
+console.log(describeValue("hello"));
+console.log(describeValue(""));
+console.log(describeValue(25));
+console.log(describeValue(0));
+console.log(describeValue(true));
+console.log(describeValue(null));
+console.log(describeValue(undefined));
+console.log(describeValue("0"));
+console.log(describeValue(NaN));
