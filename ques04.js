90700ff9dff57cb4b3423afac52828d2c8ee717d
Mushfiq-Azam <mushfiq.azam7860@gmail.com>
2026-09-02 22:06:59 +0600

    refactor getCngFare function to use consistent variable naming for fare calculation

--- ques04.js
+++ ques04.js
@@ -1,18 +1,18 @@
 
 function getCngFare(distance, isNight = false, waitingMinutes = 0) {
-    let fare = 50;
+    let f = 50;
 
     if (distance > 2) {
-        fare = fare + (distance - 2) * 15;
+        f = f + (distance - 2) * 15;
     }
 
-    fare = fare + waitingMinutes * 2;
+    f = f + waitingMinutes * 2;
 
     if (isNight) {
-        fare = fare * 1.20;
+        f = f * 1.20;
     }
 
-    return fare;
+    return f;
 }
 
 
